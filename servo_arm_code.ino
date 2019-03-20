24a39579811d8225e6e45f1d60e3bfd33c2875c5
mehCat4 <dianne_numkena@yahoo.com>
2019-03-20 11:44:01 -0700

    4 servo code
    Actual code for 4 servos, other one was 3 servos lol

--- servo_arm_code.ino
+++ servo_arm_code.ino
@@ -7,6 +7,7 @@
 Servo servo1; //Servos
 Servo servo2;
 Servo servo3;
+Servo servo4;
 
 const int LED1 = 2; //LEDs
 const int LED2 = 3;
@@ -23,22 +24,27 @@ boolean button2Pressed = false;
 const int pot1 = A0; //Potentimeters
 const int pot2 = A1; 
 const int pot3 = A2;
+const int pot4 = A3;
 
 int pot1Val; //Potentimeter values
 int pot2Val;
 int pot3Val;
+int pot4Val;
 int pot1Angle;
 int pot2Angle;
 int pot3Angle;
+int pot4Angle;
 
 int servo1PosSaves[] = {1,1,1,1,1}; //position saves
 int servo2PosSaves[] = {1,1,1,1,1};
 int servo3PosSaves[] = {1,1,1,1,1};
+int servo4PosSaves[] = {1,1,1,1,1};
 
 void setup() {
   servo1.attach(5); // Set up everything and will run once; attach servos and define the pin modes
   servo2.attach(6);
   servo3.attach(9);
+  servo4.attach(10);
   
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
@@ -59,10 +65,13 @@ void loop() {
   pot2Angle = map(pot2Val, 0, 1023, 0, 179);
   pot3Val = analogRead(pot3);
   pot3Angle = map(pot3Val, 0, 1023, 0, 179);
+  pot4Val = analogRead(pot4);
+  pot4Angle = map(pot3Val, 0, 1023, 0, 179);
   
   servo1.write(pot1Angle); // These will make the servos move to the mapped angles
   servo2.write(pot2Angle);
   servo3.write(pot3Angle);
+  servo4.write(pot4Angle);
   
   if(digitalRead(button1) == HIGH){ // This will check how many times button1 is pressed and save the positions to an array depending on how many times it is pressed; switch/case works like a if statement
     button1Presses++;
@@ -71,6 +80,7 @@ void loop() {
         servo1PosSaves[0] = pot1Angle;
         servo2PosSaves[0] = pot2Angle;
         servo3PosSaves[0] = pot3Angle;
+        servo4PosSaves[0] = pot4Angle;
         digitalWrite(LED1, HIGH);
         Serial.println("Pos 1 Saved");
         break;
@@ -78,6 +88,7 @@ void loop() {
          servo1PosSaves[1] = pot1Angle;
          servo2PosSaves[1] = pot2Angle;
          servo3PosSaves[1] = pot3Angle;
+         servo4PosSaves[1] = pot4Angle;
          digitalWrite(LED2, HIGH);
          Serial.println("Pos 2 Saved");
          break;
@@ -85,6 +96,7 @@ void loop() {
          servo1PosSaves[2] = pot1Angle;
          servo2PosSaves[2] = pot2Angle;
          servo3PosSaves[2] = pot3Angle;
+         servo4PosSaves[2] = pot4Angle;
          digitalWrite(LED3, HIGH);
          Serial.println("Pos 3 Saved");
          break;
@@ -92,6 +104,7 @@ void loop() {
          servo1PosSaves[3] = pot1Angle;
          servo2PosSaves[3] = pot2Angle;
          servo3PosSaves[3] = pot3Angle;
+         servo4PosSaves[3] = pot4Angle;
          digitalWrite(LED4, HIGH);
          Serial.println("Pos 4 Saved");
          break;
@@ -99,6 +112,7 @@ void loop() {
          servo1PosSaves[4] = pot1Angle;
          servo2PosSaves[4] = pot2Angle;
          servo3PosSaves[4] = pot3Angle;
+         servo4PosSaves[4] = pot4Angle;
          digitalWrite(LED5, HIGH);
          Serial.println("Pos 5 Saved");
          break;
@@ -114,10 +128,12 @@ void loop() {
         servo1.write(servo1PosSaves[i]);
         servo2.write(servo2PosSaves[i]);
         servo3.write(servo3PosSaves[i]);
+        servo4.write(servo4PosSaves[i]);
         Serial.println(" potentimeter Angles: ");
         Serial.println(servo1PosSaves[i]);
         Serial.println(servo2PosSaves[i]);
         Serial.println(servo3PosSaves[i]);
+        Serial.println(servo4PosSaves[i]);
         delay(1050);
       }
   }
